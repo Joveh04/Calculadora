1e98fe612ec801f04b4e51e877fbfae583cdb3ec
jonathan Caceres <joveh04@gmail.com>
2022-09-22 02:11:13 +0200

    se agrega el proyecto

---
+++ app.js
@@ -0,0 +1,24 @@
+function limpiar(){
+    document.getElementById(`calculadora`).reset();
+}
+
+function sumar(){
+    let x= parseInt(document.getElementById(`valor1`).value);
+    let y= parseInt(document.getElementById(`valor2`).value);
+    document.getElementById(`resultado`).innerHTML= x + y
+}
+function restar(){
+    let x= parseInt(document.getElementById(`valor1`).value);
+    let y= parseInt(document.getElementById(`valor2`).value);
+    document.getElementById(`resultado`).innerHTML= x - y
+}
+function multiplicar(){
+    let x= parseInt(document.getElementById(`valor1`).value);
+    let y= parseInt(document.getElementById(`valor2`).value);
+    document.getElementById(`resultado`).innerHTML= x * y
+}
+function dividir(){
+    let x= parseInt(document.getElementById(`valor1`).value);
+    let y= parseInt(document.getElementById(`valor2`).value);
+    document.getElementById(`resultado`).innerHTML= x / y
+}
